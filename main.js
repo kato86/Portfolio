12a399ef717404a79c3c7a6869597ee45106765e
MATRIX\Kamil <tomala.kamil@gmail.com>
2018-10-22 19:43:42 +0200

    Dodawanie i usuwanie tekstów Designer, Developer, Creator na Start

--- main.js
+++ main.js
@@ -169,3 +169,55 @@ function animate() {
 animate();
 
 // koniec Tło Gwiazdy
+
+const TypeWriter = function(txtElement, words) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = 3000;
+    this.type();
+    this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function() {
+
+    const current = this.wordIndex % this.words.length;
+
+    const fullTxt = this.words[current];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        typeSpeed = this.wait;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 500;
+    }
+   
+    setTimeout(() => this.type(), typeSpeed)
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const txtElement = document.querySelector('.txt-change');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    new TypeWriter(txtElement, words, wait);
+}
